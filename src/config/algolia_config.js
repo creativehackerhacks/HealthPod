80f8eea53f78865ccf240ddc9895976c8aaa7a4d
Ansh <creativehackerhacks@gmail.com>
2021-08-06 08:47:40 +0530

    Fixed exposed keys

--- src/config/algolia_config.js
+++ src/config/algolia_config.js
@@ -1,7 +1,7 @@
 // For the default version
 import algoliasearch from "algoliasearch";
 
-const client = algoliasearch("4JUHLDA2RV", "9c6b5e6365cf2b8192ebc84ce43f4a1f");
+const client = algoliasearch("4JUHLDA2RVAN", "9c6b5e6365cf2b8192ebc84ce43f4a1fSH");
 const index = client.initIndex("diseases");
 
 const algoliaConfig = index;
